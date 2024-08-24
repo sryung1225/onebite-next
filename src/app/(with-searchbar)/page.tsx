50e2124a683a09420cfa48c1a4d486ba1af52888
sryung <sryung1225@gmail.com>
2024-08-24 18:23:24 +0900

    [🥽 test] 클라이언트 컴포넌트에서 서버 컴포넌트 import 결과 확인
    - 구조) Home > ClientComponent > ServerComponent
    - ServerComponent가 클라이언트 컴포넌트로서 작동해 "서버 컴포넌트!" 가 브라우저 콘솔에 출력됨을 확인

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,5 +1,11 @@
+import ClientComponent from "./client-component";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <div className={styles.page}>인덱스 페이지</div>;
+  return (
+    <div className={styles.page}>
+      인덱스 페이지
+      <ClientComponent />
+    </div>
+  );
 }

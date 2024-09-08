3ec031247627d6a1ce01ca719986091364439aa7
sryung <sryung1225@gmail.com>
2024-09-08 15:48:15 +0900

    [✨ feat] SearchBar 컴포넌트 기능 보완
    - useSearchParams Hook을 활용하여 현재 페이지에 전달된 q라는 이름의 query값을 가져와 저장. q의 값이 변할 때마다 업데이트
    - 입력 값이 없거나 이전과 같은 값이 반복 입력된 경우는 업데이트하지 않도록 방어
    - 입력 후 enter 키다운 이벤트 적용

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -1,21 +1,39 @@
 "use client";
 
-import { useState } from "react";
-import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 import style from "./serachbar.module.css";
 
 export default function Searchbar() {
-  const [search, setSearch] = useState("");
   const router = useRouter();
+
+  const searchParams = useSearchParams();
+  const q = searchParams.get("q");
+
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
+
   const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
+
   const onSubmit = () => {
+    if (!search || q === search) return;
     router.push(`/search?q=${search}`);
   };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
+
   return (
     <div className={style.container}>
-      <input value={search} onChange={onChangeSearch} />
+      <input value={search} onChange={onChangeSearch} onKeyDown={onKeyDown} />
       <button type="button" onClick={onSubmit}>
         검색
       </button>

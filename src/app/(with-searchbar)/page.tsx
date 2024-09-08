72ee0ee980a9a5418b6531cada992d42318f7f62
sryung <sryung1225@gmail.com>
2024-09-08 15:27:14 +0900

    [⏪ revert] 서버/클라이언트 컴포넌트 테스트용 코드 원복

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,14 +1,5 @@
-import ClientComponent from "../../components/client-component";
-import ServerComponent from "../../components/server-component";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      인덱스 페이지
-      <ClientComponent>
-        <ServerComponent />
-      </ClientComponent>
-    </div>
-  );
+  return <div className={styles.page}>인덱스 페이지</div>;
 }
--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,16 +1,7 @@
-import ClientComponent from "@/components/client-component";
-
 export default function Page({
   searchParams,
 }: {
   searchParams: { q?: string };
 }) {
-  return (
-    <div>
-      Search 페이지 {searchParams.q}
-      <ClientComponent>
-        <></>
-      </ClientComponent>
-    </div>
-  );
+  return <div>Search 페이지 {searchParams.q}</div>;
 }
